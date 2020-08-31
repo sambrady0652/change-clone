92484a674e7846a2b89755f3843ad7beeb6c2037
Zackitty <59539209+Zackitty@users.noreply.github.com>
2020-08-31 15:50:44 -0500

    route changs

---
+++ app/api/routes/updates.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, jsonify
+from app.models import Update
+
+bp = Blueprint('updates', __name__)
+
+
+@bp.route('/<update_id>', methods=['GET', 'PATCH', 'DELETE'])
+def get_update (update_id):
+  # Get Specific Update Based On Id
+    response = Update.query.get(update_id)
+    return {"updates": [update.to_dict() for update in response]}
+
+@bp.route('/<update_id>/comments', methods=['GET', 'POST'])
+def get_comments(update_id):
+  update_response = Update.comments.query.filter_by(update_id=update_id)
+  return {"updates": [update.to_dict() for update in update_response] }
+
+def add_comment():
+    print(dir(request.data))
+    data = request.form
+    new_comment = Comment(update_id=data['update_id'], body=data['body'], user_id=data['user_id'])
+    db.session.add(new_comment)
+    db.session.commit()
+
+
