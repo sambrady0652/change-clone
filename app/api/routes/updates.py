b8f419882b08cdf564e5711d081ae88927a4c095
Zackitty <59539209+Zackitty@users.noreply.github.com>
2020-08-31 18:13:45 -0500

    route changs

--- app/api/routes/updates.py
+++ app/api/routes/updates.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Blueprint, jsonify
 from app.models import Update
 
@@ -11,12 +10,18 @@ def get_update (update_id):
     response = Update.query.get(update_id)
     return {"updates": [update.to_dict() for update in response]}
 
+def edit_update(update_id):
+  #Edit A Specific Update
+  
+
 @bp.route('/<update_id>/comments', methods=['GET', 'POST'])
+#Get Comments From Specific Update
 def get_comments(update_id):
   update_response = Update.comments.query.filter_by(update_id=update_id)
   return {"updates": [update.to_dict() for update in update_response] }
 
 def add_comment():
+    # Write Comment On Specific Update
     print(dir(request.data))
     data = request.form
     new_comment = Comment(update_id=data['update_id'], body=data['body'], user_id=data['user_id'])
@@ -24,22 +29,3 @@ def add_comment():
     db.session.commit()
 
 
-=======
-from flask import Blueprint, jsonify
-from app.models import Update
-
-bp = Blueprint('updates', __name__)
-
-
-@bp.route('/<update_id>', methods=('GET', 'PATCH', 'DELETE'))
-def get_update (update_id):
-    response = Update.query.get(update_id)
-    return {"updates": [update.to_dict() for update in response]}
-
-@bp.route('/<update_id>/comments', methods=('GET', 'POST'))
-def get_comments():
-  response = Update.comments.query.all()
-  return {"updates": [update.to_dict() for update in response] }
-
-# def add_comment():
->>>>>>> 29002bf49de32eed3c8c83d86d4f1b5491ecdefb
