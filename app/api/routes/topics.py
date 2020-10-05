1648ab4e3cba9838317c94a88053ff7670b5151e
Zackitty <59539209+Zackitty@users.noreply.github.com>
2020-10-04 19:55:08 -0500

    fix routes

--- app/api/routes/topics.py
+++ app/api/routes/topics.py
@@ -4,7 +4,7 @@ from app.models import Topic
 
 bp = Blueprint('topics', __name__)
 
-@bp.route('/')
+@bp.route('')
 def get_topics():
     topics = Topic.query.all()
     return {str(topic.id): topic.to_dict() for topic in topics}
