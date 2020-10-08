a0f55b8bde3561f29d4a02466c19f91dba747b65
Zackitty <59539209+Zackitty@users.noreply.github.com>
2020-10-08 15:02:01 -0500

    Add faviconand fixroutes

--- app/__init__.py
+++ app/__init__.py
@@ -43,6 +43,6 @@ def inject_csrf_token(response):
 @app.route('/', defaults={'path': ''})
 @app.route('/<path>')
 def react_root(path):
-    if path == 'favicon.ico':
-        return app.send_static_file('favicon.ico')
+    if path == 'cause.ico':
+        return app.send_static_file('cause.ico')
     return app.send_static_file('index.html')
